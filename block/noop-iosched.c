996d8c8cc1e13807cf15903f9545f76457dd0b48
Qian Sun <qiansun@10-249-157-157.wireless.oregonstate.edu>
2018-05-06 19:18:48 -0700

    Test

--- block/noop-iosched.c
+++ block/noop-iosched.c
@@ -18,6 +18,8 @@ static void noop_merged_requests(struct request_queue *q, struct request *rq,
 	list_del_init(&next->queuelist);
 }
 
+
+
 static int noop_dispatch(struct request_queue *q, int force)
 {
 	struct noop_data *nd = q->elevator->elevator_data;
@@ -58,6 +60,8 @@ noop_latter_request(struct request_queue *q, struct request *rq)
 		return NULL;
 	return list_entry(rq->queuelist.next, struct request, queuelist);
 }
+
+
 
 static int noop_init_queue(struct request_queue *q, struct elevator_type *e)
 {
